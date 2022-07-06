751bd37be6eaa87b40bb478c6f7602dc61c5bfe8
Justas Belevičius <84323002+Justas1988@users.noreply.github.com>
2022-07-06 14:22:00 +0300

    Update main.py
    added input and some waste of time features

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from random import randint #importing randint() 
+from random import randint
+from time import sleep
 
 def random_string_generator():
 
@@ -17,7 +18,6 @@ def random_string_generator():
 
 	print(f'unique_key_string: {key[0]}{key[1]}{key[2]}-{key[3]}{key[4]}{key[5]}-{key[6]}')
 
-	
 
 def random_string_printer(i):
 	while i != 0:
@@ -26,8 +26,30 @@ def random_string_printer(i):
 		print(f'unique_key_string: {key[0]}{key[1]}{key[2]}-{key[3]}{key[4]}{key[5]}-{key[6]}')
 
 		i = i-1
+		sleep(0.5) #for waisting time for no reason
 
-random_string_printer(5)
+
+def input_trigger():
+	print()
+	print ('Enter how many strings do you want?')
+	print()
+	sleep(1)
+	string_number = int(input('Enter an Integer: '))
+	print()	
+	sleep(1)
+	print ('You entered ' + str(string_number))
+	print()
+	sleep(1)
+	print ('Printing ' + str(string_number) + ' random strings get ready:')
+	sleep(1)
+	print()
+	random_string_printer(string_number)
+
+
+input_trigger()
+
+print()
+print()
 print('end of code')
 
 
