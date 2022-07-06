1599fb91a2b50706c62c2b2251d6d338ee8b8ed7
Justas Belevičius <84323002+Justas1988@users.noreply.github.com>
2022-07-06 13:51:56 +0300

    Update main.py
    redone code using functions

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from random import randint #importing randint() 
 
-letters_list = ['a','b','c','d','e','f','g']
-i = 50
-while not i==0:
+def random_string_generator():
+
+	letters_list = ['a','b','c','d','e','f','g']
 
 	n1 = randint(0,6)
 	n2 = randint(0,6)
@@ -12,12 +12,22 @@ while not i==0:
 	n6 = randint(0,6)
 	n7 = randint(0,6)
 
-	key = (letters_list[n1],letters_list[n2],letters_list[n3],letters_list[n4],letters_list[n5],letters_list[n6],letters_list[n7])
-	
+	key_tuple = (letters_list[n1],letters_list[n2],letters_list[n3],letters_list[n4],letters_list[n5],letters_list[n6],letters_list[n7])
+	return key_tuple
 
 	print(f'unique_key_string: {key[0]}{key[1]}{key[2]}-{key[3]}{key[4]}{key[5]}-{key[6]}')
 
-	i = i - 1
+	
 
-print('end')
-print(type(key))
+def random_string_printer(i):
+	while i != 0:
+		key = random_string_generator()
+
+		print(f'unique_key_string: {key[0]}{key[1]}{key[2]}-{key[3]}{key[4]}{key[5]}-{key[6]}')
+
+		i = i-1
+
+random_string_printer(5)
+print('end of code')
+
+
