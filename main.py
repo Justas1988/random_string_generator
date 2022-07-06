cf8d2d0871552aa0331c304eab4d10810efdb47d
Justas Belevičius <84323002+Justas1988@users.noreply.github.com>
2022-07-06 13:07:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
-import random
+from random import randint #importing randint() 
 
 letters_list = ['a','b','c','d','e','f','g']
 i = 50
 while not i==0:
 
-	n1 = random.randint(0,6)
-	n2 = random.randint(0,6)
-	n3 = random.randint(0,6)
-	n4 = random.randint(0,6)
-	n5 = random.randint(0,6)
-	n6 = random.randint(0,6)
-	n7 = random.randint(0,6)
+	n1 = randint(0,6)
+	n2 = randint(0,6)
+	n3 = randint(0,6)
+	n4 = randint(0,6)
+	n5 = randint(0,6)
+	n6 = randint(0,6)
+	n7 = randint(0,6)
 
 	key = (letters_list[n1],letters_list[n2],letters_list[n3],letters_list[n4],letters_list[n5],letters_list[n6],letters_list[n7])
 	
