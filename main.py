63b691c63c8f06a40c90f9d8a1d17a7392e2b8fe
Justas Belevičius <84323002+Justas1988@users.noreply.github.com>
2022-07-07 22:59:47 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
 from random import randint
 from time import sleep
 
+def rand():
+	number = randint(0.6)
+	return number
+
 def random_string_generator():
 
 	letters_list = ['a','b','c','d','e','f','g']
-
+	'''
 	n1 = randint(0,6)
 	n2 = randint(0,6)
 	n3 = randint(0,6)
 	n4 = randint(0,6)
 	n5 = randint(0,6)
 	n6 = randint(0,6)
-	n7 = randint(0,6)
-
-	key_tuple = (letters_list[n1],letters_list[n2],letters_list[n3],letters_list[n4],letters_list[n5],letters_list[n6],letters_list[n7])
+	n7 = randint(0,6) 
+	'''
+	key_tuple = (letters_list[rand()],letters_list[rand()],letters_list[rand()],letters_list[rand()],letters_list[rand()],letters_list[rand()],letters_list[rand()])
 	return key_tuple
 
 	print(f'unique_key_string: {key[0]}{key[1]}{key[2]}-{key[3]}{key[4]}{key[5]}-{key[6]}')
